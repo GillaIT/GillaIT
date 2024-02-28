a49810d2e33e5eced5128a9cc0f4d747c5049a3a
안정근 <anjeong8577@naver.com>
2024-02-29 06:06:26 +0900

    #6 feat: 로그인 로직

--- src/main/java/com/example/GillaIT/web/HomeController.java
+++ src/main/java/com/example/GillaIT/web/HomeController.java
@@ -1,9 +1,12 @@
 package com.example.GillaIT.web;
 
+import com.example.GillaIT.domain.member.Member;
 import com.example.GillaIT.domain.post.PostService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class HomeController {
@@ -15,7 +18,10 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
+            Model model) {
+
         model.addAttribute("posts", postService.getPosts());
         return "home";
     }
