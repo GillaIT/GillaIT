b624bc5e6ab715f410f13b5c19341f9465929042
mingdodev <mingdo.dev@gmail.com>
2024-02-29 08:46:11 +0900

    #7 feat: code 생성 및 저장 중

---
+++ src/main/java/com/example/GillaIT/domain/email/CodeRepository.java
@@ -0,0 +1,13 @@
+package com.example.GillaIT.domain.email;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CodeRepository {
+
+
+    private void save(String code, String email) {
+
+    }
+    public String findCodeByEmail()
+}
