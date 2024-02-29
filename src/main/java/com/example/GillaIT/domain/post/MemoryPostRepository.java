929ee7248d75667708155a3a95fbf4e6bff8d394
ywonchae1 <ywonchae62@gmail.com>
2024-02-29 09:23:35 +0900

    #24 feat: 게시글 생성 기능 DB와 연결

--- src/main/java/com/example/GillaIT/domain/post/MemoryPostRepository.java
+++ src/main/java/com/example/GillaIT/domain/post/MemoryPostRepository.java
@@ -7,23 +7,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
-public class MemoryPostRepository implements PostRepository {
+public class MemoryPostRepository {
     private static List<Post> store = new ArrayList<>();
     private static Long sequence = 0L;
 
-    @Override
+//    @Override
     public List<Post> findAll() {
         return store;
     }
 
-    @Override
+//    @Override
     public void save(Post post) {
         post.setId(sequence++);
         store.add(post);
     }
 
-    @Override
+//    @Override
     public Optional<Post> findById(Long id) {
         return store.stream().filter(post -> post.getId().equals(id)).findAny();
     }
--- src/main/java/com/example/GillaIT/domain/post/PostRepository.java
+++ src/main/java/com/example/GillaIT/domain/post/PostRepository.java
@@ -1,12 +1,11 @@
 package com.example.GillaIT.domain.post;
 
 import com.example.GillaIT.domain.entity.Post;
+import com.example.GillaIT.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface PostRepository {
-    List<Post> findAll();
-    void save(Post post);
-    Optional<Post> findById(Long id);
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
