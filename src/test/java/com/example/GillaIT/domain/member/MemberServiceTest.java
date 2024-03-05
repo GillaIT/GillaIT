f30c33b1b6f1f80f8b8e97091f6a84dfc975253b
ggom121@naver.com <ggom121@naver.com>
2024-03-05 15:03:59 +0900

    feat: 회원삭제 테스트코드 추가

--- src/test/java/com/example/GillaIT/domain/member/MemberServiceTest.java
+++ src/test/java/com/example/GillaIT/domain/member/MemberServiceTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
@@ -20,18 +19,36 @@ class MemberServiceTest {
 
     @Test
     void is_active토글() {
+        //given
         Member member = new Member();
         member.setEmail("test@test.com");
         member.setPassword("password");
 
+        //when
         memberRepository.save(member);
 
+        //then
         memberService.toggleIsActive(member.getId());
         assertThat(member.getIs_active()).isEqualTo(true);
 
         memberService.toggleIsActive(member.getId());
         assertThat(member.getIs_active()).isEqualTo(false);
 
+    }
+
+    @Test
+    void 회원삭제() {
+        //given
+        Member member = new Member();
+        member.setEmail("test@test.com");
+        member.setPassword("password");
+
+        //when
+        memberRepository.save(member);
+
+        //then
+        memberService.deleteById(member.getId());
+        assertThat(memberRepository.find(member.getId())).isEqualTo(null);
     }
 
 
