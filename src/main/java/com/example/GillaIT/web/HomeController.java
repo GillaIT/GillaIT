4c1e37b9ed99659a1e395d05b0860e2d2f801f44
ywonchae1 <ywonchae62@gmail.com>
2024-03-26 16:54:14 +0900

    #41 feat: 로그인했다면 글 작성 버튼 보이기

--- src/main/java/com/example/GillaIT/web/HomeController.java
+++ src/main/java/com/example/GillaIT/web/HomeController.java
@@ -3,6 +3,7 @@ package com.example.GillaIT.web;
 import com.example.GillaIT.domain.member.Member;
 import com.example.GillaIT.domain.post.PostService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,10 +20,17 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(
+            HttpServletRequest request,
             @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
             Model model) {
 
         model.addAttribute("posts", postService.getPosts());
+
+        // 로그인 했는지 확인 -> 글 작성 버튼 보이게
+        HttpSession session = request.getSession();
+        if(session != null & session.getAttribute(SessionConst.LOGIN_MEMBER) != null) {
+            model.addAttribute("loggedIn", true);
+        }
         return "home";
     }
 }
