366023b9d74e8c83361c3185d18365b8466909c0
ywonchae1 <ywonchae62@gmail.com>
2024-03-26 16:53:53 +0900

    #41 feat: 로그인 사용자의 작성 글인지 확인, 버튼 보이기

--- src/main/java/com/example/GillaIT/web/post/PostController.java
+++ src/main/java/com/example/GillaIT/web/post/PostController.java
@@ -1,15 +1,23 @@
 package com.example.GillaIT.web.post;
 
 import com.example.GillaIT.domain.entity.Post;
+import com.example.GillaIT.domain.member.Member;
 import com.example.GillaIT.domain.post.PostService;
+import com.example.GillaIT.web.SessionConst;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 @Controller
 @RequestMapping("/post")
+@Slf4j
 public class PostController {
     private PostService postService;
     public PostController(PostService postService) {
@@ -22,15 +30,24 @@ public class PostController {
     }
 
     @PostMapping("")
-    public String postCreateProcess(@ModelAttribute("post") Post post) {
+    public String postCreateProcess(HttpServletRequest request, @ModelAttribute("post") Post post) {
+        HttpSession session = request.getSession(false);
+        Member member = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        post.setMember(member);
         postService.savePost(post);
         return "redirect:/";
     }
 
     @GetMapping("/detail/{postId}")
-    public String postDetail(@PathVariable("postId") Long postId, Model model) {
+    public String postDetail(HttpServletRequest request, @PathVariable("postId") Long postId, Model model) {
         Post post = postService.getPost(postId);
         model.addAttribute("post", post);
+        // 로그인 사용자의 작성글인지 확인
+        HttpSession session = request.getSession();
+        if(post.getMember().equals(session.getAttribute(SessionConst.LOGIN_MEMBER))) { // Member Class에 equals를 overriding 해주어야 한다
+            model.addAttribute("writer", true);
+        }
+
         return "post/postDetail";
     }
 
