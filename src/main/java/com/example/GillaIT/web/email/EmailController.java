09ab1880b8ad731e295af1436d4b74280314ff0f
mingdodev <mingdo.dev@gmail.com>
2024-05-08 02:42:58 +0900

    #53 feat: Implement email authentication service (Checking authNum)

--- src/main/java/com/example/GillaIT/web/email/EmailController.java
+++ src/main/java/com/example/GillaIT/web/email/EmailController.java
@@ -9,32 +9,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 @RequiredArgsConstructor
 public class EmailController {
     private final EmailSendService emailSendService;
 
     // Send Email: 인증번호 전송 버튼 click
     @PostMapping("/signup/email")
-    public String mailSend(@RequestBody @Valid EmailRequestDto emailRequestDto) {
-        System.out.println("이메일 인증 요청" + emailRequestDto.getEmail());
-        return emailSendService.joinEmail(emailRequestDto.getEmail());
+    public Map<String, String> mailSend(@RequestBody @Valid EmailRequestDto emailRequestDto) {
+        String code = emailSendService.joinEmail(emailRequestDto.getEmail());
+        Map<String, String> response = new HashMap<>();
+        response.put("code", code);
+
+        return response;
     }
 
     // Email Auth: 인증번호 입력 후 인증 버튼 click
     @PostMapping("/signup/emailAuth")
-    public String authCheck(@RequestParam @Valid EmailCheckDto emailCheckDto) {
+    public String authCheck(@RequestBody @Valid EmailCheckDto emailCheckDto) {
         Boolean checked = emailSendService.checkAuthNum(emailCheckDto.getEmail(), emailCheckDto.getAuthNum());
         if (checked) {
-            return "ok";
+            return "이메일 인증 성공!";
         }
         else {
-            // 에러 처리는 안 할래 -.-
-            throw new NullPointerException("우와 ~ 잘못됨");
+            throw new NullPointerException("잘못된 인증번호이거나 인증번호가 만료되었습니다.");
         }
-
     }
-
-
 }
