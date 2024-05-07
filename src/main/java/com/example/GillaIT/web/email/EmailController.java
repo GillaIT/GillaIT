4676fe68f3ba1c48e4d5687d93042c88b4af7644
mingdodev <mingdo.dev@gmail.com>
2024-05-07 22:15:01 +0900

    #53 feat: Get email value as emailRequestDto object

--- src/main/java/com/example/GillaIT/web/email/EmailController.java
+++ src/main/java/com/example/GillaIT/web/email/EmailController.java
@@ -2,10 +2,12 @@ package com.example.GillaIT.web.email;
 
 import com.example.GillaIT.domain.email.EmailSendService;
 import com.example.GillaIT.dtos.EmailCheckDto;
+import com.example.GillaIT.dtos.EmailRequestDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -15,10 +17,9 @@ public class EmailController {
 
     // Send Email: 인증번호 전송 버튼 click
     @PostMapping("/signup/email")
-    public String mailSend(@RequestParam(name = "email") @Valid String email) {
-        System.out.println(email);
-//        System.out.println("이메일 인증 요청" + emailDto.getEmail());
-        return emailSendService.joinEmail(email);
+    public String mailSend(@RequestBody @Valid EmailRequestDto emailRequestDto) {
+        System.out.println("이메일 인증 요청" + emailRequestDto.getEmail());
+        return emailSendService.joinEmail(emailRequestDto.getEmail());
     }
 
     // Email Auth: 인증번호 입력 후 인증 버튼 click
