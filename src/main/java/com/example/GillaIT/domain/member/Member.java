2f9d6d0d00d6aff6f61d4936f9be07830c55f97e
mingdodev <mingdo.dev@gmail.com>
2024-06-25 04:36:55 +0900

    #55 회원가입 폼 유효성 검사 (필수 정보)

--- src/main/java/com/example/GillaIT/domain/member/Member.java
+++ src/main/java/com/example/GillaIT/domain/member/Member.java
@@ -1,6 +1,10 @@
 package com.example.GillaIT.domain.member;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,17 +22,26 @@ public class Member {
     @Column(name = "id")
     private Long id;
 
+    @NotBlank(message = "   * 그룹 이름을 입력해주세요.")
     @Column
     private String group_name;
 
+    @NotBlank(message = "   * 이메일을 입력해주세요.")
+    @Email(message = "   * 올바른 형식의 이메일을 입력해주세요.")
     @Column
     private String email;
+
+    @NotNull
+    @Size(min = 8, message="   * 8자리 이상의 비밀번호를 입력해주세요.")
     @Column
     private String password;
+
     @Column
     private String image;
+
     @Column
     private Boolean is_admin;
+
     @Column
     private Boolean is_active;
 
